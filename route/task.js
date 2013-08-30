c4996cfe543778b558c7e307c4a079fb3df78e0d
Rowan Manning <rowan.manning@nature.com>
2013-08-30 16:18:26 +0100

    Spaces to tabs

--- route/task.js
+++ route/task.js
@@ -3,75 +3,75 @@
 var Hapi = require('hapi');
 
 var pathValidator = {
-    id: Hapi.types.String().alphanum()
+	id: Hapi.types.String().alphanum()
 };
 
 function notImplemented (req) {
-    req.reply().code(501);
+	req.reply().code(501);
 }
 
 // Routes relating to individual tasks
 module.exports = [
 
-    // Get a task
-    {
-        method: 'GET',
-        path: '/tasks/{id}',
-        handler: notImplemented,
-        config: {
-            validate: {
-                query: {},
-                payload: false,
-                path: pathValidator
-            }
-        }
-    },
+	// Get a task
+	{
+		method: 'GET',
+		path: '/tasks/{id}',
+		handler: notImplemented,
+		config: {
+			validate: {
+				query: {},
+				payload: false,
+				path: pathValidator
+			}
+		}
+	},
 
-    // Delete a task
-    {
-        method: 'DELETE',
-        path: '/tasks/{id}',
-        handler: notImplemented,
-        config: {
-            validate: {
-                query: {},
-                payload: false,
-                path: pathValidator
-            }
-        }
-    },
+	// Delete a task
+	{
+		method: 'DELETE',
+		path: '/tasks/{id}',
+		handler: notImplemented,
+		config: {
+			validate: {
+				query: {},
+				payload: false,
+				path: pathValidator
+			}
+		}
+	},
 
-    // Get results for a task
-    {
-        method: 'GET',
-        path: '/tasks/{id}/results',
-        handler: notImplemented,
-        config: {
-            validate: {
-                query: {
-                    from: Hapi.types.String().date(),
-                    to: Hapi.types.String().date(),
-                    full: Hapi.types.Boolean()
-                },
-                payload: false,
-                path: pathValidator
-            }
-        }
-    },
+	// Get results for a task
+	{
+		method: 'GET',
+		path: '/tasks/{id}/results',
+		handler: notImplemented,
+		config: {
+			validate: {
+				query: {
+					from: Hapi.types.String().date(),
+					to: Hapi.types.String().date(),
+					full: Hapi.types.Boolean()
+				},
+				payload: false,
+				path: pathValidator
+			}
+		}
+	},
 
-    // Generate results for a task
-    // TODO: use 202 status
-    {
-        method: 'GET',
-        path: '/tasks/{id}/run',
-        handler: notImplemented,
-        config: {
-            validate: {
-                query: {},
-                payload: false,
-                path: pathValidator
-            }
-        }
-    }
+	// Generate results for a task
+	// TODO: use 202 status
+	{
+		method: 'GET',
+		path: '/tasks/{id}/run',
+		handler: notImplemented,
+		config: {
+			validate: {
+				query: {},
+				payload: false,
+				path: pathValidator
+			}
+		}
+	}
 
 ];
--- route/tasks.js
+++ route/tasks.js
@@ -3,74 +3,74 @@
 var Hapi = require('hapi');
 
 function notImplemented (req) {
-    req.reply().code(501);
+	req.reply().code(501);
 }
 
 // Routes relating to all tasks
 module.exports = [
 
-    // Get all tasks
-    {
-        method: 'GET',
-        path: '/tasks',
-        handler: notImplemented,
-        config: {
-            validate: {
-                query: {},
-                payload: false
-            }
-        }
-    },
+	// Get all tasks
+	{
+		method: 'GET',
+		path: '/tasks',
+		handler: notImplemented,
+		config: {
+			validate: {
+				query: {},
+				payload: false
+			}
+		}
+	},
 
-    // Create a task
-    // TODO: return 201 status on success
-    {
-        method: 'POST',
-        path: '/tasks',
-        handler: notImplemented,
-        config: {
-            validate: {
-                query: {},
-                payload: {
-                    url: Hapi.types.String().required(),
-                    standard: Hapi.types.String().required().valid([
-                        'Section508', 'WCAG2A', 'WCAG2AA', 'WCAG2AAA'
-                    ]),
-                    ignore: Hapi.types.Array()
-                }
-            }
-        }
-    },
+	// Create a task
+	// TODO: return 201 status on success
+	{
+		method: 'POST',
+		path: '/tasks',
+		handler: notImplemented,
+		config: {
+			validate: {
+				query: {},
+				payload: {
+					url: Hapi.types.String().required(),
+					standard: Hapi.types.String().required().valid([
+						'Section508', 'WCAG2A', 'WCAG2AA', 'WCAG2AAA'
+					]),
+					ignore: Hapi.types.Array()
+				}
+			}
+		}
+	},
 
-    // Get results for all tasks
-    {
-        method: 'GET',
-        path: '/tasks/results',
-        handler: notImplemented,
-        config: {
-            validate: {
-                query: {
-                    from: Hapi.types.String().date(),
-                    to: Hapi.types.String().date(),
-                    full: Hapi.types.Boolean()
-                },
-                payload: false
-            }
-        }
-    },
+	// Get results for all tasks
+	{
+		method: 'GET',
+		path: '/tasks/results',
+		handler: notImplemented,
+		config: {
+			validate: {
+				query: {
+					from: Hapi.types.String().date(),
+					to: Hapi.types.String().date(),
+					full: Hapi.types.Boolean()
+				},
+				payload: false
+			}
+		}
+	},
 
-    // Generate new results for all tasks
-    // TODO: return 202 status
-    {
-        method: 'GET',
-        path: '/tasks/run',
-        handler: notImplemented,
-        config: {
-            validate: {
-                query: {},
-                payload: false
-            }
-        }
-    }
+	// Generate new results for all tasks
+	// TODO: return 202 status
+	{
+		method: 'GET',
+		path: '/tasks/run',
+		handler: notImplemented,
+		config: {
+			validate: {
+				query: {},
+				payload: false
+			}
+		}
+	}
 
 ];
