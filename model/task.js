7bc51d8b4a65a9be9dd505922079144e8bc5378a
Rayraegah <severanrye@live.com>
2016-05-21 13:18:06 +0100

    increasing maxcomplexity to 7 for model/task.js
    Until there’s a better cleaner method

--- model/task.js
+++ model/task.js
@@ -12,6 +12,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with pa11y-webservice.  If not, see <http://www.gnu.org/licenses/>.
+/*jshint maxcomplexity:7*/
 
 'use strict';
 
