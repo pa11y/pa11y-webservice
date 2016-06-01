6bd03da5562b2b04d3611fbe0daef821b8c1a3e1
Rowan Manning <info@rowanmanning.com>
2016-06-01 20:49:57 +0100

    Rename all the repos and uppercase Pa11y

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// This file is part of pa11y-webservice.
+// This file is part of Pa11y Webservice.
 //
-// pa11y-webservice is free software: you can redistribute it and/or modify
+// Pa11y Webservice is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// pa11y-webservice is distributed in the hope that it will be useful,
+// Pa11y Webservice is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with pa11y-webservice.  If not, see <http://www.gnu.org/licenses/>.
+// along with Pa11y Webservice.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
@@ -26,13 +26,13 @@ process.on('SIGINT', function() {
 
 require('./app')(config, function(err, app) {
 	console.log('');
-	console.log(chalk.underline.cyan('pa11y-webservice started'));
+	console.log(chalk.underline.cyan('Pa11y Webservice started'));
 	console.log(chalk.grey('mode: %s'), env);
 	console.log(chalk.grey('uri:  %s'), app.server.info.uri);
 
 	if (err) {
 		console.error('');
-		console.error(chalk.red('Error starting pa11y-webservice:'));
+		console.error(chalk.red('Error starting Pa11y Webservice:'));
 		console.error(err.message);
 	}
 });
