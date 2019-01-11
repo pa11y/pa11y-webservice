d3b197316f269302887ce68e68df4f8ccad5a022
Jose Bolos <joseluisbolos@gmail.com>
2019-01-11 15:20:25 +0000

    Adding debug info to mongo connection

--- app.js
+++ app.js
@@ -39,6 +39,23 @@ function initApp(config, callback) {
 		function(next) {
 			/* eslint camelcase: 'off' */
 			MongoClient.connect(config.database, {server: {auto_reconnect: false}}, function(error, db) {
+				if(error) {
+					console.log('Error connecting to MongoDB:');
+					console.log(JSON.stringify(error));
+				}
+
+				db.on('timeout', () => {
+					console.log('Mongo connection timeout');
+				})
+
+				db.on('close', () => {
+					console.log('Mongo connection closed');
+				})
+
+				db.on('reconnect', () => {
+					console.log('Mongo reconnected');
+				})
+
 				app.db = db;
 				next(error);
 			});
