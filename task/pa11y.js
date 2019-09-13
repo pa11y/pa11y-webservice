b278bad1d6e14ceb3d08958eb0de1a3bec227eda
Jose Bolos <joseluisbolos@gmail.com>
2019-09-13 13:46:09 +0100

    Refactor async.queue behaviour affected by upgrade (#93)
    * Refactors out async.waterfall, correctly uses model.task.getAll()
    * Renames functions and updates comments

--- task/pa11y.js
+++ task/pa11y.js
@@ -23,60 +23,49 @@ exports.runPa11yOnTasks = runPa11yOnTasks;
 
 // Initialise the task
 function initTask(config, app) {
-
 	if (!config.cron) {
 		config.cron = '0 30 0 * * *'; // 00:30 daily
 	}
-	const job = new CronJob(config.cron, run.bind(null, app));
+	const job = new CronJob(config.cron, taskRunner.bind(null, app));
 	job.start();
-
 }
 
-// Run the task
-function run(app) {
-
+// Runs the task
+async function taskRunner(app) {
 	console.log('');
-	console.log(chalk.grey('Starting to run tasks @ %s'), new Date());
+	console.log(chalk.grey('Starting to run task @ %s'), new Date());
 
-	async.waterfall([
-
-		next => app.model.task.getAll(next),
-
-		(tasks, next) => runPa11yOnTasks(tasks, app, next)
-
-	], error => {
-
-		if (error) {
-			console.error(chalk.red('Failed to run tasks: %s'), error.message);
-			console.log('');
-			process.exit(1);
-		}
-
-	});
-
+	try {
+		const tasks = await app.model.task.getAll();
+		runPa11yOnTasks(tasks, app);
+	} catch (error) {
+		console.error(chalk.red('Failed to run task: %s'), error.message);
+		console.log('');
+		process.exit(1);
+	}
 }
 
-// Run Pa11y on an array of tasks
-function runPa11yOnTasks(tasks, app, done) {
+// Runs an array of pa11y tasks
+function runPa11yOnTasks(tasks, app) {
+	if (tasks.length === 0) {
+		console.log('No pa11y tasks to run');
+		return;
+	}
 
-	const queue = async.queue((task, nextInQueue) => {
-		console.log('Starting task %s', task.id);
-		app.model.task.runById(task.id, error => {
-			if (error) {
-				console.log(chalk.red('Failed to finish task %s: %s'), task.id, error.message);
-			} else {
-				console.log(chalk.green('Finished task %s'), task.id);
-			}
-			nextInQueue();
-		});
-
-	}, 2);
-
-	queue.drain = () => {
-		console.log(chalk.grey('Finished running tasks @ %s'), new Date());
-		done();
+	const worker = async task => {
+		console.log('Starting pa11y task %s', task.id);
+		try {
+			await app.model.task.runById(task.id);
+			console.log(chalk.green('Finished pa11y task %s'), task.id);
+		} catch (error) {
+			console.log(chalk.red('Failed to finish pa11y task %s: %s'), task.id, error.message);
+		}
 	};
 
+	const queue = async.queue(worker, 2);
 	queue.push(tasks);
 
+	queue.drain(() => {
+		console.log(chalk.grey('Finished running pa11y tasks @ %s'), new Date());
+	});
 }
