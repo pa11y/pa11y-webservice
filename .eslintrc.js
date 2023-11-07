19e42bab443a84a9ab065f67c9dea732135ea0cf
Danyal Aytekin <danyal@alienpaper.com>
2023-11-07 21:21:52 +0000

    Remove object shorthand override, since it only produces a warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@
 const config = require('pa11y-lint-config/eslint/es2017');
 
 // Temporary overrides for this project
-config.rules['object-shorthand'] = 'off';
 config.rules['prefer-arrow-callback'] = 'off';
 
 module.exports = config;
