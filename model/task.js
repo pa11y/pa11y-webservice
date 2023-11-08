230cf710a1c6251e40f678554cd7e39ee90ec130
Danyal Aytekin <danyal@alienpaper.com>
2023-11-08 19:08:19 +0000

    Remove unnecessary leniency for `id-length`

--- model/task.js
+++ model/task.js
@@ -13,7 +13,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Pa11y Webservice.  If not, see <http://www.gnu.org/licenses/>.
 
-/* eslint id-length: 'off' */
 /* eslint no-catch-shadow: 'off' */
 /* eslint no-underscore-dangle: 'off' */
 /* eslint new-cap: 'off' */
